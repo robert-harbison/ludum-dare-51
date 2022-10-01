4a90d9598b57a9dc386faa71457d938830a7ac16
samqchau <67344952+samqchau@users.noreply.github.com>
2022-10-01 16:50:26 -0700

    projectiles kill zombies

--- ludum-dare-51/Assets/Scripts/Enemies/Zombie.cs
+++ ludum-dare-51/Assets/Scripts/Enemies/Zombie.cs
@@ -7,11 +7,12 @@ public class Zombie : MonoBehaviour {
 
     private GameObject player;
     public NavMeshAgent agent;
-
     public float range = 1f;
+    private float health;
 
 	private void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        health = 10;
 	}
 
 	private void Update() {
@@ -19,9 +20,15 @@ public class Zombie : MonoBehaviour {
 
        if (dist > range) {
             agent.destination = player.transform.position;
-	    } else {
+	   } else {
             player.SendMessage("ZombieBite");
             Destroy(gameObject);
-		}
+	   }
+        if (health <= 0) Destroy(gameObject);
+    }
+
+    public void DamageZombie(float damage)
+    {
+        health -= damage;
     }
 }
--- ludum-dare-51/Assets/Scripts/PlayerController.cs
+++ ludum-dare-51/Assets/Scripts/PlayerController.cs
@@ -46,7 +46,6 @@ public class PlayerController : MonoBehaviour
 	{
 		Move();
 		Rotate();
-
 		HandleForcefield();
 	}
 
